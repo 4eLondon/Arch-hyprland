da813eee1f9f13e11a81444fc53cf2a3f0b65a57
4eLondon <rhyncc@gmail.com>
2026-08-19 02:19:15 -0500

    Init: Added script to set up user filesystem

---
+++ scripts/configure_files.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e "\n+ + [ Now creating user directories ] + + \n"
+
+mkdir ~/{Pictures,Downloads,Videos,Desktop,Music,Documents}
+
+read -p "Create custom subfolders? (y/n): " choice
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    echo "- Placing custom subfolders  -"
+    mkdir -p ~/Pictures/{Screenshots/{Sorted,Unsorted},Wallpapers,Saved}
+    sleep 1s
+    mkdir ~/Music/{Songs,Sounds}
+    sleep 1s
+    mkdir ~/Videos/{Saved,Unsorted}
+    sleep 1s
+    mkdir -p ~/Documents/{Projects/{Active,Temp,Logs,Idle},Papers/{Notes,Guides},Misc}
+    echo -e "\n+ + [ Done ] + + \n"
+    echo  -e "\n+ + [ Folders and subfolders added successfully ] + +\n"
+else
+    echo -e "\n+ + [ Done ] + + \n"
+    echo -e "\n+ + [ All Folders added successfully ] + +\n"
+fi
