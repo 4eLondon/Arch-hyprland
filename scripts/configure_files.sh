8cf6e13af86d5ae49c1982e474b0c5a03036b66f
4eLondon <rhyncc@gmail.com>
2026-08-19 19:34:31 -0500

    Refactor: Futher edits to file script error handling

--- scripts/configure_files.sh
+++ scripts/configure_files.sh
@@ -3,16 +3,16 @@ set -euo pipefail
 
 echo -e "\n+ + [ Now creating user directories ] + + \n"
 
-mkdir ~/{Pictures,Downloads,Videos,Desktop,Music,Documents}
+mkdir -p ~/{Pictures,Downloads,Videos,Desktop,Music,Documents}
 
 read -p "Create custom subfolders? (y/n): " choice
 if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
     echo "- Placing custom subfolders  -"
     mkdir -p ~/Pictures/{Screenshots/{Sorted,Unsorted},Wallpapers,Saved}
     sleep 1s
-    mkdir ~/Music/{Songs,Sounds}
+    mkdir -p ~/Music/{Songs,Sounds}
     sleep 1s
-    mkdir ~/Videos/{Saved,Unsorted}
+    mkdir -p ~/Videos/{Saved,Unsorted}
     sleep 1s
     mkdir -p ~/Documents/{Projects/{Active,Temp,Logs,Idle},Papers/{Notes,Guides},Misc}
     echo -e "\n+ + [ Done ] + + \n"
